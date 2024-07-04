10461b29933700b3a7c4826d31c5d2ecf277c7ae
sandeepr1537 <164096457+sandeepr1537@users.noreply.github.com>
2024-07-05 07:45:35 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,21 +6,21 @@ provider "azurerm" {
 }
 
 # Create the subscription
-resource "azurerm_subscription" "new_subscription" {
+resource "azurerm_subscription" "subscription" {
   name              = var.subscription_name
   billing_scope_id  = var.billing_account_id
   management_group_id = var.management_group_id
 }
 
 # Apply policies as needed
-resource "azurerm_policy_assignment" "example_policy" {
+resource "azurerm_policy_assignment" "policy" {
   name               = "policy-assignment"
   scope              = azurerm_subscription.new_subscription.id
   policy_definition_id = var.policy_definition_id 
 }
 
 # Apply RBAC roles as needed
-resource "azurerm_role_assignment" "example_role" {
+resource "azurerm_role_assignment" "role" {
   scope                = azurerm_subscription.new_subscription.id
   role_definition_name = "Contributor"
   principal_id         = var.principal_id
